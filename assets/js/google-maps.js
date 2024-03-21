53ea8ba766010f41b68a08f6481b2fab443ef5a9
Dev-nawaz <145113225+Dev-nawaz@users.noreply.github.com>
2024-03-22 03:43:34 +0530

    Deployment Portfolio

---
+++ assets/js/google-maps.js
@@ -0,0 +1,15 @@
+// function initMap() {
+//     // Latitude and Longitude
+//     var myLatLng = {lat: 26.193705, lng: 84.324465};
+
+//     var map = new google.maps.Map(document.getElementById('google-maps'), {
+//         zoom: 17,
+//         center: myLatLng
+//     });
+
+//     var marker = new google.maps.Marker({
+//         position: myLatLng,
+//         map: map,
+//         title: 'Pratappur' // Title Location
+//     });
+// }
